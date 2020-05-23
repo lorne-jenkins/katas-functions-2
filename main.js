1646b97752737bd5dba090b45dc4744b36a609b3
Lorne J <biglornej@gmail.com>
2020-05-23 09:05:53 -0600

    Replaced the ++ operation with add function

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function add(a, b) {
 }
 function multiply(a, b) {
     let result = a
-    for (i = 1; i < b; i++) {
+    for (i = 1; i < b; i=add(i,1)) {
         result = add(result, a)
     }
     return result
@@ -15,7 +15,7 @@ console.log('Second step:', multiply(5, 4))
 
 function power(x, n) {
     let result = x
-    for (let index = 1; index < n; index++) {
+    for (let index = 1; index < n; index=add(index,1)) {
         result = multiply(result, x)
     }
     return result
@@ -26,7 +26,7 @@ console.log('Exponent example', power(3, 2))
 
 function factorial(x) {
     let factorialFactor = x
-    for (index = 1; index < x; index++) {
+    for (index = 1; index < x; index=add(index,1)) {
        factorialFactor = multiply(factorialFactor,add(x,-index))
     }
     return factorialFactor
@@ -42,7 +42,7 @@ if (x <=2) {
     let fNminus1 = 1
     let fNminus2 = 0
     let fN
-    for (let index = 0 ; index <add(x,-2); index++) {
+    for (let index = 0 ; index <add(x,-2); index=add(index,1)) {
         fN = add(fNminus1,fNminus2)
         fNminus2 = fNminus1
         fNminus1 = fN
